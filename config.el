e66627d07681cca7914086e922a12d4e2ece66e7
Evan Lecklider <evan@lecklider.com>
2019-09-20 13:13:05 -0700

    Initial commit

---
+++ config.el
@@ -0,0 +1,35 @@
+;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
+
+;; Fira Code all day.
+(setq doom-font (font-spec :family "Fira Code" :size 11))
+
+;; Slightly smoother scrolling.
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1) ((control) . nil)))
+
+;; Always start with a maximized frame (window in the OS).
+(add-hook 'window-setup-hook #'toggle-frame-maximized)
+
+;; Load my default theme.
+(setq doom-theme 'doom-nord)
+
+;; Tell Projectile to recompile tags on a regular interval.
+(setq projectile-enable-idle-timer t)
+(setq projectile-idle-timer-seconds 300)
+
+;; Tell Projectile to look in ~/Code and ~/Clones for projects.
+(setq projectile-project-search-path '("~/Code" "~/Clones"))
+
+;; Add _ as a word character in Ruby.
+(add-hook 'enh-ruby-mode-hook '(lambda () (modify-syntax-entry ?_ "w")))
+
+;; Stop bothering me about this shit.
+(setq tags-revert-without-query t)
+
+;; Like vim's magic relative line numbers.
+(setq-default display-line-numbers-type 'relative)
+
+;; Easier window movement.
+(map! :n "C-h" #'evil-window-left
+      :n "C-j" #'evil-window-down
+      :n "C-k" #'evil-window-up
+      :n "C-l" #'evil-window-right)
---
+++ packages.el
@@ -0,0 +1,8 @@
+;; -*- no-byte-compile: t; -*-
+;;; ~/.doom.d/packages.el
+(package! dockerfile-mode)
+
+;;; Examples:
+;; (package! some-package)
+;; (package! another-package :recipe (:fetcher github :repo "username/repo"))
+;; (package! builtin-package :disable t)
