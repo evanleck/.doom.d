57e29093667e9cefb229d25cbcb3e7d9afaf0e21
Evan Lecklider <evan@lecklider.com>
2020-04-07 10:56:22 -0700

    Organize config.el

--- config.el
+++ config.el
@@ -1,35 +1,17 @@
 ;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
+(setq doom-font (font-spec :family "JetBrains Mono" :size 11))
 
-;; Fira Code all day.
-(setq doom-font (font-spec :family "SF Mono" :size 11))
-
-;; Slightly smoother scrolling.
+;; Slightly more sane scrolling.
 (setq mouse-wheel-scroll-amount '(1 ((shift) . 1) ((control) . nil)))
-
-;; Always start with a maximized frame (window in the OS).
-(add-hook 'window-setup-hook #'toggle-frame-maximized)
 
 ;; Load my default theme.
 (setq doom-theme 'doom-dracula)
 
-;; Tell Projectile to recompile tags on a regular interval.
-(setq projectile-enable-idle-timer t)
-(setq projectile-idle-timer-seconds 300)
-
-;; Enable simpleclip
-(simpleclip-mode 1)
-
-;; Enable evil-matchit
-(global-evil-matchit-mode 1)
-
-;; Tell Projectile to look in ~/Code and ~/Clones for projects.
-(setq projectile-project-search-path '("~/Code" "~/Code/prepaid" "~/Code/prepaid/wt" "~/Code/clones"))
-
-;; Add _ as a word character in Ruby.
-(add-hook 'enh-ruby-mode-hook '(lambda () (modify-syntax-entry ?_ "w")))
-
 ;; Stop bothering me about this shit.
 (setq tags-revert-without-query t)
+
+;; Don't keep other tags tables.
+(setq tags-add-tables nil)
 
 ;; Like vim's magic relative line numbers.
 (setq-default display-line-numbers-type 'relative)
@@ -39,3 +21,33 @@
       :n "C-j" #'evil-window-down
       :n "C-k" #'evil-window-up
       :n "C-l" #'evil-window-right)
+
+;; Always start with a maximized frame (window in the OS).
+(add-hook 'window-setup-hook #'toggle-frame-maximized)
+
+(add-hook! ruby-mode
+  ;; Add _ as a word character.
+  (modify-syntax-entry ?_ "w")
+  ;; Use etags for ruby-mode.
+  (set-company-backend! 'ruby-mode 'company-etags))
+
+(add-hook! web-mode
+  ;; Add _ as a word character.
+  (modify-syntax-entry ?_ "w"))
+
+(add-hook! js2-mode
+  ;; Add _ as a word character.
+  (modify-syntax-entry ?_ "w"))
+
+;; Matchit
+(global-evil-matchit-mode +1)
+
+;; Tell Projectile to look in ~/Code and ~/Clones for projects.
+(setq projectile-project-search-path '("~/Code" "~/Code/prepaid" "~/Code/prepaid/wt" "~/Code/clones"))
+
+;; Tell Projectile to recompile tags on a regular interval.
+;; (setq projectile-enable-idle-timer t)
+;; (setq projectile-idle-timer-seconds 300)
+
+;; Good god, stop copying everything I delete!
+(simpleclip-mode +1)
